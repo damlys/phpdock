c5c5ba3b1964d8e07e5e10cbd76dff491db27f75
damlys <damian.lysiak@gmail.com>
2022-01-20 08:19:30 +0100

    Terraform #2

--- terraform.tf
+++ terraform.tf
@@ -23,3 +23,36 @@ terraform {
     }
   }
 }
+
+locals {
+  gcp_project_id     = "phpdock-0"
+  gcp_project_name   = "phpdock"
+  gcp_default_region = "europe-central2"
+  gcp_default_zone   = "europe-central2-a"
+}
+
+provider "google" {
+  project = local.gcp_project_id
+  region  = local.gcp_default_region
+  zone    = local.gcp_default_zone
+}
+
+/**********************************************
+*
+* Project
+*
+***********************************************/
+
+data "google_billing_account" "damlys_dev" {
+  display_name = "Damian Łysiak Dev"
+  open         = true
+}
+resource "google_project" "this" {
+  project_id      = local.gcp_project_id
+  name            = local.gcp_project_name
+  billing_account = data.google_billing_account.damlys_dev.id
+}
+resource "google_project_service" "container" {
+  project = google_project.this.project_id
+  service = "container.googleapis.com"
+}
