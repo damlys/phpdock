f345c19a6fa57b50fc99114e3870968ae8159864
Damian Łysiak <damian.lysiak@gmail.com>
2025-01-21 16:55:42 +0100

    feature: replace Caddy with Nginx
    reverts https://github.com/damlys/phpdock/commit/f24485275329e7953645b16949de57aeb5d15578

---
+++ docker-images/rte/bin/entrypoint.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+inject_environment_variables() {
+  envsubst $(printenv | cut -f1 -d'=' | sed 's/.*/\\\${&}/' | tr '\n' ',')
+}
+
+if [[ "$(id --user)" == '0' ]]; then
+  echo 'Generating the "/etc/nginx/nginx.conf" file.' >"$NGINX_ERROR_LOG"
+  inject_environment_variables </etc/nginx/nginx.template.conf >/etc/nginx/nginx.conf
+else
+  echo 'Skipping generation of the "/etc/nginx/nginx.conf" file.' >"$NGINX_ERROR_LOG"
+fi
+
+exec "$@"
